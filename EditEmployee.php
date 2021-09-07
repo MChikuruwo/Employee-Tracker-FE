c819751e7bc19e8d6c13742946d546dd0dbe5279
Munyaradzi Chikuruwo <mchikuruwo@hotmail.com>
2021-09-07 22:55:57 +0200

    Pages added, remeining edition

--- EditEmployee.php
+++ EditEmployee.php
@@ -3,19 +3,67 @@
 ?> 
 	<div class="main-wrapper">
 
-		<!-- partial:partials/_sidebar.html -->
-		<?php 
+			<!-- partial:partials/_sidebar.html -->
+			<?php 
       include('partials/_sidebar.html')
     ?> 
 		<!-- partial -->
 	
 		<div class="page-wrapper">
 					
-			
+        <?php 
+            include('partials/_topnav.html')
+        ?> 
+
+<div class="page-content">
+
+					<div class="col-md-12 grid-margin stretch-card">
+						<div class="card">
+							<div class="card-body">
+								<h6 class="card-title">Edit Employee</h6>
+								<form class="forms-sample">
+									<div class="form-group row">
+										<label for="exampleInputUsername2" class="col-sm-3 col-form-label">Username</label>
+										<div class="col-sm-9">
+											<input type="text" class="form-control" id="exampleInputUsername2" placeholder="Email">
+										</div>
+									</div>
+									<div class="form-group row">
+										<label for="exampleInputEmail2" class="col-sm-3 col-form-label">Email</label>
+										<div class="col-sm-9">
+											<input type="email" class="form-control" id="exampleInputEmail2" autocomplete="off" placeholder="Email">
+										</div>
+									</div>
+									<div class="form-group row">
+										<label for="exampleInputMobile" class="col-sm-3 col-form-label">Mobile</label>
+										<div class="col-sm-9">
+											<input type="number" class="form-control" id="exampleInputMobile" placeholder="Mobile number">
+										</div>
+									</div>
+									<div class="form-group row">
+										<label for="exampleInputPassword2" class="col-sm-3 col-form-label">Password</label>
+										<div class="col-sm-9">
+											<input type="password" class="form-control" id="exampleInputPassword2" autocomplete="off" placeholder="Password">
+										</div>
+									</div>
+									<div class="form-check form-check-flat form-check-primary mt-0">
+										<label class="form-check-label">
+											<input type="checkbox" class="form-check-input">
+											Remember me
+										</label>
+									</div>
+									<button type="submit" class="btn btn-primary mr-2">Submit</button>
+									<button class="btn btn-light">Cancel</button>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+				
 
 			<!-- partial:partials/_footer.html -->
 			<footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between">
-            <p class="text-muted text-center text-md-left">Copyright © 2021 <a href="https://www.aemaps.co.zw" target="_blank">Employee Tracker</a>. All rights reserved</p>
+				<p class="text-muted text-center text-md-left">Copyright © 2021 <a href="https://www.aemaps.co.zw" target="_blank">Employee Tracker</a>. All rights reserved</p>
 				<p class="text-muted text-center text-md-left mb-0 d-none d-md-block">Handcrafted With <i class="mb-1 text-primary ml-1 icon-small" data-feather="heart"></i></p>
 			</footer>
 			<!-- partial -->
---
+++ tasks.php
@@ -0,0 +1,166 @@
+<?php 
+  include('includes/head.php')
+?> 
+	<div class="main-wrapper">
+
+			<!-- partial:partials/_sidebar.html -->
+			<?php 
+      include('partials/_sidebar.html')
+    ?> 
+		<!-- partial -->
+	
+		<div class="page-wrapper">
+					
+        <?php 
+            include('partials/_topnav.html')
+        ?> 
+
+<div class="page-content">
+<div class="row">
+					<div class="col-lg-12 grid-margin stretch-card">
+						<div class="card">
+							<div class="card-body">
+								<h4 class="card-title">Employee Tasks</h4>
+								<div class="table-responsive">
+									<table class="table table-striped">
+										<thead>
+											<tr>
+												<th>User</th>
+												<th>Name</th>
+												<th>Progress</th>
+												<th>Salary</th>
+												<th>Start date</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td class="py-1">
+													<img src="http://via.placeholder.com/36x36" alt="image">
+												</td>
+												<td>Cedric Kelly</td>
+												<td>
+													<div class="progress">
+														<div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+													</div>
+												</td>
+												<td>$206,850</td>
+												<td>June 21, 2021</td>
+											</tr>
+											<tr>
+												<td class="py-1">
+													<img src="http://via.placeholder.com/36x36" alt="image">
+												</td>
+												<td>Haley Kennedy</td>
+												<td>
+													<div class="progress">
+														<div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+													</div>
+												</td>
+												<td>$313,500</td>
+												<td>May 15, 2021</td>
+											</tr>
+											<tr>
+												<td class="py-1">
+													<img src="http://via.placeholder.com/36x36" alt="image">
+												</td>
+												<td>Bradley Greer</td>
+												<td>
+													<div class="progress">
+														<div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" role="progressbar" style="width: 90%" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+													</div>
+												</td>
+												<td>$132,000</td>
+												<td>Apr 12, 2021</td>
+											</tr>
+											<tr>
+												<td class="py-1">
+													<img src="http://via.placeholder.com/36x36" alt="image">
+												</td>
+												<td>Brenden Wagner</td>
+												<td>
+													<div class="progress">
+														<div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+													</div>
+												</td>
+												<td>$206,850</td>
+												<td>June 21, 2021</td>
+											</tr>
+											<tr>
+												<td class="py-1">
+													<img src="http://via.placeholder.com/36x36" alt="image">
+												</td>
+												<td>Bruno Nash</td>
+												<td>
+													<div class="progress">
+														<div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar" style="width: 35%" aria-valuenow="35" aria-valuemin="0" aria-valuemax="100"></div>
+													</div>
+												</td>
+												<td>$163,500</td>
+												<td>January 01, 2021</td>
+											</tr>
+											<tr>
+												<td class="py-1">
+													<img src="http://via.placeholder.com/36x36" alt="image">
+												</td>
+												<td>Sonya Frost</td>
+												<td>
+													<div class="progress">
+														<div class="progress-bar progress-bar-striped progress-bar-animated bg-info" role="progressbar" style="width: 65%" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+													</div>
+												</td>
+												<td>$103,600</td>
+												<td>July 18, 2021</td>
+											</tr>
+											<tr>
+												<td class="py-1">
+													<img src="http://via.placeholder.com/36x36" alt="image">
+												</td>
+												<td>Zenaida Frank</td>
+												<td>
+													<div class="progress">
+														<div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100"></div>
+													</div>
+												</td>
+												<td>$313,500</td>
+												<td>March 22, 2021</td>
+											</tr>
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+
+			<!-- partial:partials/_footer.html -->
+			<footer class="footer d-flex flex-column flex-md-row align-items-center justify-content-between">
+				<p class="text-muted text-center text-md-left">Copyright © 2021 <a href="https://www.aemaps.co.zw" target="_blank">Employee Tracker</a>. All rights reserved</p>
+				<p class="text-muted text-center text-md-left mb-0 d-none d-md-block">Handcrafted With <i class="mb-1 text-primary ml-1 icon-small" data-feather="heart"></i></p>
+			</footer>
+			<!-- partial -->
+		
+		</div>
+	</div>
+
+	<!-- core:js -->
+	<script src="assets/vendors/core/core.js"></script>
+	<!-- endinject -->
+  <!-- plugin js for this page -->
+  <script src="assets/vendors/chartjs/Chart.min.js"></script>
+  <script src="assets/vendors/jquery.flot/jquery.flot.js"></script>
+  <script src="assets/vendors/jquery.flot/jquery.flot.resize.js"></script>
+  <script src="assets/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
+  <script src="assets/vendors/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendors/progressbar.js/progressbar.min.js"></script>
+	<!-- end plugin js for this page -->
+	<!-- inject:js -->
+	<script src="assets/vendors/feather-icons/feather.min.js"></script>
+	<script src="assets/js/template.js"></script>
+	<!-- endinject -->
+  <!-- custom js for this page -->
+  <script src="assets/js/dashboard.js"></script>
+  <script src="assets/js/datepicker.js"></script>
+	<!-- end custom js for this page -->
+</body>
+</html>    
